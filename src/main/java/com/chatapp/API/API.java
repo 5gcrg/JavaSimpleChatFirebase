b8ff41fedf564bf79f9bc035569a85ba57c3c948
5gcrg <admirecrush@gmail.com>
2025-08-06 22:41:15 +0800

    removed my APIKEY

--- src/main/java/com/chatapp/API/API.java
+++ src/main/java/com/chatapp/API/API.java
@@ -3,7 +3,7 @@ package com.chatapp.API;
 public class API {
 
     // get your own
-    public static final String API_KEY = "AIzaSyAjcdDB_XfDv9gL3KFuK-uoirF6ULyihx8";
+    public static final String API_KEY = "";
     public static final String DATABASE_URL = "https://chatapp-47909-default-rtdb.firebaseio.com/";
 
 }
